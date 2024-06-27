cda2ca1207af9be1656a4a2befa4b4e287f8d447
phatbt <btphat.dev@gmail.com>
2024-06-27 15:29:29 +0700

    first commit

---
+++ src/main/java/com/btphat/readbookeveryday/ReadbookeverydayApplication.java
@@ -0,0 +1,13 @@
+package com.btphat.readbookeveryday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadbookeverydayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadbookeverydayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/btphat/readbookeveryday/ReadbookeverydayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btphat.readbookeveryday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadbookeverydayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
